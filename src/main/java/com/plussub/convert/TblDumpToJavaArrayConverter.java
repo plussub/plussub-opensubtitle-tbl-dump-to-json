694200ffb0912100efeefd7b7e0871920d8e9ff7
ste-xx <stefan.breitenstein@gmail.com>
2017-03-05 13:30:44 +0100

    add java array converter

--- src/main/java/com/plussub/convert/TblDumpToJavaArrayConverter.java
+++ src/main/java/com/plussub/convert/TblDumpToJavaArrayConverter.java
@@ -1,7 +1,6 @@
 package com.plussub.convert;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.stream.Collector;
@@ -19,24 +18,16 @@ public class TblDumpToJavaArrayConverter implements TblDumpTo {
         return perLine
                 .map(TblDumpTo::lineToIso639)
                 .map(TblDumpToJavaArrayConverter::writeAsJavaInit)
-                .collect(TblDumpToJavaArrayConverter.joinAsJavaArray());
+                .collect(TblDumpToJavaArrayConverter.joinAsJavaArrayInit());
     }
 
-    private static Collector<CharSequence, ?, String> joinAsJavaArray() {
+    private static Collector<CharSequence, ?, String> joinAsJavaArrayInit() {
         return  Collectors.joining(",", "{", "}");
     }
 
 
      static String writeAsJavaInit(Iso639Entry entry){
-        Iso639Entry[] example = new Iso639Entry[]{new Iso639Entry("a","b","c"),};
-
         return String.format("new Iso639Entry(\"%s\",\"%s\",\"%s\")",entry.getValue(),entry.getValueAs2Char(),entry.getName());
-
-//        try {
-//            return new ObjectMapper().writeValueAsString(ref);
-//        } catch (JsonProcessingException e) {
-//            throw new RuntimeException(e);
-//        }
     }
 
 }
